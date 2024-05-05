1333bc1cbaaab717bf72d1948e3e6d882c2e4fea
unknown <nabeerasheed@gmail.com>
2024-05-05 21:12:42 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { BrowserRouter, Route, Routes,Link } from 'react-router-dom';
+import Students from './Students';
+import Favstudent from './fstudents';
+import "./index.css"
+import { useState } from 'react';
 
 function App() {
+  const [studentarr,setstudent]=useState([{id:1,name:"Bharath",disabled: false},{id:2,name:"Kumar",disabled: false},{id:3,name:"Ramya",disabled: false},{id:4,name:"Monica",disabled: false},{id:5,name:"Shruthi",disabled: false}])
+    
+    const [favarr,setfavstudent]=useState([])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <BrowserRouter>
+    <nav className='bg-red-500 m-2 p-2 text-xl flex gap-4 underline'>
+      <Link to={"/"}>List of Students</Link>
+      {"    "}
+      <Link to={"/about"}>Favourite Students</Link>
+    </nav>
+    <Routes>
+    <Route path='/' element={<Students studentarr={studentarr} favarr={favarr} setfavstudent={setfavstudent} setstudent={setstudent}/>}></Route>
+   <Route path='/about' element={<Favstudent  studentarr={studentarr} favarr={favarr} setfavstudent={setfavstudent} setstudent={setstudent}/>}></Route>
+   </Routes>
+   </BrowserRouter>
     </div>
-  );
-}
+  )}
+    
+
 
 export default App;
